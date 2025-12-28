c730fd040e75a37fdb1ebd609db942d0fd5e056b
Marcus D. Duer <marcus0duer@gmail.com>
2025-12-28 17:31:29 +0100

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,89 @@
+
+/* 
+===========================
+Create Database and Schemas
+=========================== 
+
+Script Purpose: This script creates a new database called "DataWarehouse", 
+and three schemas called "bronze, silver and gold" following the Medallion Architecture.
+
+Warning: Running this script will drop the entire 'DataWarehouse' database if it exists. 
+All data will therefor be lost and permanently deleted. Proceed with caution, and ensure 
+proper backups  before running this script.
+
+*/
+
+USE master;
+GO
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1
+FROM sys.databases
+WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP  DATABASE DataWarehouse;
+END;
+GO
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
